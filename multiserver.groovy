87391a393572a474894b9f4475316a976226b8df
pandu1031 <chintupandu1031@gmail.com>
2023-06-07 22:50:39 +0530

    deploy pipeline

---
+++ multiserver.groovy
@@ -0,0 +1,26 @@
+//Declarative pipeline
+pipeline{
+    agent any
+    parameters {
+        string(name: 'SERVERIP', defaultValue: '', description: '')
+         string(name: 'BUILDNUM', defaultValue: '', description: '')
+          string(name: 'BRANCH', defaultValue: '', description: '') 
+}
+stages{
+    stage("Multiple sever deployment"){
+        steps{
+
+            sh '''
+            aws s3 cp s3://mamuu/pandu/${BRANCH}/${BUILDNUM}/hello-${BUILDNUM}.war .
+            IFS=, read -ra input <<< "${SERVERIP}"
+            for ip in {$input[@]}
+            do
+            echo "$ip"
+            scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem hello-${BUILDNUM}.war ec2-user@$ip:/var/lib/tomcat/webapps
+            ssh -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem ec2-user@$ip "hostname"
+            done
+            '''
+        }
+    }
+}
+}
